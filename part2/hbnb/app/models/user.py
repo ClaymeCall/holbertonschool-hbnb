dbf1ee519ec11dc3ef14a1c7b357f2d5d59165ba
Clement <9397@holbertonstudents.com>
2024-10-21 08:26:31 +0200

    Changed regex pattern

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 '''
 This module defines the Business Logic User class.
 '''
@@ -5,7 +6,7 @@ from base_model import BaseModel
 import re
 
 # This regex is used to verify valid emails.
-regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+regex = r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$'
 
 
 class User(BaseModel):
