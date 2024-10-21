0e6e6ca812fc7fce7d9a6efe063a70688c0066a5
GenXwater <louis.genty@outlook.com>
2024-10-21 15:55:15 +0200

    added class amenity

---
+++ part2/hbnb/app/models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+This module defines the Business Logic User class.
+'''
+from base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    def __init__(self, name):
+        super().__init__()
+        
+        if len(name) > 50:
+            raise ValueError("Name must be 50 chars maximum")
+        else:
+            self.name = name
