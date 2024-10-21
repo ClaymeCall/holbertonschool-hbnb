7ba36079ba1f75b4789d409ebb7547f0ce29e0e7
Bomays <9314@holbertonstudents.com>
2024-10-21 16:15:23 +0200

    deleted the private and protected setting of ID and rating, and herited Review from BaseModel SuperClass

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""This model defines the Business review class"""
 import uuid
 from datetime import datetime
+from part2.hbnb.app.models.base_model import BaseModel
 from part2.hbnb.app.models import place
 
 
@@ -8,11 +10,12 @@ users_list = []
 places_list = []
 
 
-class Review:
+class Review(BaseModel):
     def __init__(self, place, user, rating, text):
-        self.__id = str(uuid.uuid4())  # Unique Id generated of the review
+        super().__init__()
+        self.id = str(uuid.uuid4())  # Unique Id generated of the review
         self.text = text
-        self._rating = None  # First initialized to None
+        self.rating = rating
         self.place = place  # Place being reviewed
         self.user = user  # User who wrote review
         self.created_at = datetime.now()
@@ -21,23 +24,18 @@ class Review:
         self.rating = rating
         place.add_review(self)
 
-    # Ensure privacy
-    @property
-    def id(self):
-        return self.__id
-
-    # Protected rating
+    # Rating
     @property
     def rating(self):
         """Return rating"""
-        return self._rating
+        return self.rating
 
     @rating.setter
     def rating(self, value):
         """Frame the rating"""
         if value > 0 or value > 5:
             raise ValueError("Rating must be from 0 to 5")
-        self._rating = value
+        self.rating = value
 
     # Validation process before authorizing review
     def validate_user(self, user_id):
@@ -58,18 +56,5 @@ class Review:
     def print_review(self):
         """Print the review as a readble string"""
         return "Review by {} for {}: {} > Rating: {}".format(
-            self.user.name, self.place.name, self.text, self._rating
+            self.user.name, self.place.name, self.text, self.rating
         )
-
-    # Timestamp
-    def save(self):
-        """Update the updated_at timestamp whenever the object is modified"""
-        self.updated_at = datetime.now
-
-    def update(self, data):
-        """Update the attributes of the object
-        based on the provided dictionary"""
-        for key, value in data.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-                self.save()  # Update the updated_at timestamp
