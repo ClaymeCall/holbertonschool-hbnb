f1cc7ac3836b1d86a2f19495605a77a01e0aa9c1
CoeurdArcade <coeurdarcade@gmail.com>
2024-10-24 15:35:49 +0200

    script updated due to problem changes

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,13 +1,37 @@
-from app.models.base_model import BaseModel
+#!/usr/bin/python3
+""" holds class User"""
+
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from hashlib import md5
 import re
 
 # This regex is used to verify valid emails.
 regex = r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$'
 
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-class User(BaseModel):
-    def __init__(self, first_name, last_name, email, is_admin=False):
-        super().__init__()
+    def __init__(self, first_name, last_name, email, is_admin=False, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
 
         '''Setting email value'''
         if re.fullmatch(regex, email):
@@ -25,7 +49,7 @@ class User(BaseModel):
 
         '''Setting last name value'''
         if not last_name.isalpha():
-            raise TypeError("First name must be a string of letters.")
+            raise TypeError("Last name must be a string of letters.")
         elif len(last_name) > 50:
             raise ValueError("Last name must be 50 chars maximum")
         else:
@@ -36,3 +60,10 @@ class User(BaseModel):
             raise TypeError("is_admin must be True or False.")
         else:
             self.is_admin = is_admin
+
+    def __setattr__(self, name, value):
+        """sets a password with md5 encryption"""
+        if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
+
