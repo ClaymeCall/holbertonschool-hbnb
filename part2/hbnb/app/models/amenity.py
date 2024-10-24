8dccea4ece766e187acfccec6c33861a51248195
CoeurdArcade <coeurdarcade@gmail.com>
2024-10-24 15:18:54 +0200

    script modified since didn't take count of changes

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,14 +1,19 @@
-'''
-This module defines the Business Logic Amenity class.
-'''
-from base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    def __init__(self, name):
-        super().__init__()
-        
-        if len(name) > 50:
-            raise ValueError("Name must be 50 chars maximum")
-        else:
-            self.name = name
+class Amenity(BaseModel, Base):
+    """Representation of Amenity """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
