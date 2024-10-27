735e3344c1c13ef26ce15391ee2b40f2d89b55d7
CoeurdArcade <coeurdarcade@gmail.com>
2024-10-27 23:15:05 +0100

    typo, attribute, json, error handling fixed

--- part2/hbnb/app/api/v1/places.py
+++ part2/hbnb/app/api/v1/places.py
@@ -6,19 +6,19 @@ api = Namespace("places", description='Place operations')
 
 # Define the place model for input validation and documentation
 place_model = api.model(
-        'Place',
-        {
-            'name':fields.String(required=True, description='Name of the place'),
-            'description':fields.String(required=True, description='Description of the place'),
-            'price_per_night':fields.Float(required=True, description='Price per night'),
-            'number_of_rooms':fields.Integer(required=True, description='Number of rooms'),
-            'number_of_bathrooms':fields.Integer(required=True, description='Number of bathrooms'),
-            'max_guest':fields.Integer(required=True, description='Maximum number of guests'),
-            'amenities':fields.List(fields.String, description='List of amenities')
-            },
-        )
+    'Place',
+    {
+        'name': fields.String(required=True, description='Name of the place'),
+        'description': fields.String(required=True, description='Description of the place'),
+        'price_per_night': fields.Float(required=True, description='Price per night'),
+        'number_of_rooms': fields.Integer(required=True, description='Number of rooms'),
+        'number_of_bathrooms': fields.Integer(required=True, description='Number of bathrooms'),
+        'max_guest': fields.Integer(required=True, description='Maximum number of guests'),
+        'amenities': fields.List(fields.String, description='List of amenities')
+    },
+)
 
-facade = HBnBfacade()
+facade = HBnBFacade()
 
 @api.route('/')
 class PlaceList(Resource):
@@ -27,13 +27,13 @@ class PlaceList(Resource):
     @api.response(400, 'Invalid input data')
     def post(self):
         """Register a new place"""
-        place_data = api.playload
+        place_data = api.payload
 
-    if not place_data:
-        return {"error": "Invalid input data. JSON required"}, 400
+        if not place_data:
+            return {"error": "Invalid input data. JSON required"}, 400
 
-    new_place = facade.create_place(place_data)
-    return {
+        new_place = facade.create_place(place_data)
+        return {
             "id": new_place.id,
             "name": new_place.name,
             "description": new_place.description,
@@ -42,7 +42,7 @@ class PlaceList(Resource):
             "number_of_bathrooms": new_place.number_of_bathrooms,
             "max_guest": new_place.max_guest,
             "amenities": new_place.amenities
-            }, 201
+        }, 201
 
     @api.response(200, 'List of places retrieved successfully')
     @api.response(404, "No places found")
@@ -63,17 +63,17 @@ class PlaceResource(Resource):
         """Get place details by ID"""
         place = facade.get_place(place_id)
         if not place:
-             return {"error": "Place not found"}, 404
+            return {"error": "Place not found"}, 404
         return {
-                 "id": place.id,
-                 "name": place.name,
-                 "description": place.description,
-                 "price_per_night": place.price_per_night,
-                 "number_of_rooms": place.number_of_rooms,
-                 "number_of_bathrooms": place.number_of_bathrooms,
-                 "max_guest": place.max_guest,
-                 "amenities": place.amenities
-                 }, 200
+            "id": place.id,
+            "name": place.name,
+            "description": place.description,
+            "price_per_night": place.price_per_night,
+            "number_of_rooms": place.number_of_rooms,
+            "number_of_bathrooms": place.number_of_bathrooms,
+            "max_guest": place.max_guest,
+            "amenities": place.amenities
+        }, 200
 
     @api.expect(place_model)
     @api.response(200, 'Place updated successfully')
@@ -89,15 +89,15 @@ class PlaceResource(Resource):
         if not updated_place:
             return {"error": "Place not found"}, 404
         return {
-                "id": updated_place.id,
-                "name": updated_place.name,
-                "description": updated_place.description,
-                "price_per_night": updated_place.price_per_night,
-                "number_of_rooms": updated_place.number_of_rooms,
-                "number_of_bathrooms": updated_place.number_of_bathrooms,
-                "max_guest": updated_place.max_guest,
-                "amenities": updated_place.amenities
-                }, 200
+            "id": updated_place.id,
+            "name": updated_place.name,
+            "description": updated_place.description,
+            "price_per_night": updated_place.price_per_night,
+            "number_of_rooms": updated_place.number_of_rooms,
+            "number_of_bathrooms": updated_place.number_of_bathrooms,
+            "max_guest": updated_place.max_guest,
+            "amenities": updated_place.amenities
+        }, 200
 
     @api.response(200, 'Place deleted successfully')
     @api.response(404, 'Place not found')
@@ -120,3 +120,4 @@ class PlaceReviewList(Resource):
 
         reviews = facade.get_reviews_by_place(place_id)
         return jsonify([review.__dict__ for review in reviews])
+
