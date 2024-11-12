00e4849417ba2eb7fc3c967c4fa75cddb6e52c6d
Clement <9397@holbertonstudents.com>
2024-11-12 16:08:22 +0100

    Removed useless import

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -3,10 +3,7 @@ from app.models.user import User
 from app.models.amenity import Amenity
 from app.models.place import Place
 from app.models.review import Review
-from flask_restx import Namespace
 
-
-api = Namespace("users", description="User operations")
 
 class HBnBFacade:
     def __init__(self):
