149d4920c053feba966196aac99bd2e45674a8b3
GenXwater <louis.genty@outlook.com>
2024-11-17 17:24:25 +0100

    return init initial

--- hbnb/app/__init__.py
+++ hbnb/app/__init__.py
@@ -11,39 +11,24 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
 
-authorizations = {
-    'Bearer Auth': {
-        'type': 'apiKey',
-        'in': 'header',
-        'name': 'Authorization',
-        'description': 'JWT Authorization header using the Bearer scheme. Example: "Bearer {your_token}"'
-    }
-}
-
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Add secret key for jwt
-    app.config['JWT_SECRET_KEY'] = app.config.get('SECRET_KEY', 'your_secret_key_here')
-
-    # API configuration with authorizations
-    api = Api(
-        app,
-        version='1.0',
-        title='HBnB API',
-        description='HBnB Application API',
-        security='Bearer Auth',  # Add jwt key
-        authorizations=authorizations  # API configuration with authorizations
-    )
+    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     api.add_namespace(users_ns, path='/api/v1/users')
+
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
+
     api.add_namespace(places_ns, path='/api/v1/places')
+
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+
     api.add_namespace(auth_ns, path='/api/v1/auth')
 
     bcrypt.init_app(app)
+
     jwt.init_app(app)
 
     return app
