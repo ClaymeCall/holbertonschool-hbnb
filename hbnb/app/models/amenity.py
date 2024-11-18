22c1ffc3fa0cbf3a1429d0216f027fd48db5d642
Bomays <9314@holbertonstudents.com>
2024-11-18 15:40:10 +0100

    Adapted amenity class file for SQlalchemy db manager

--- hbnb/app/models/amenity.py
+++ hbnb/app/models/amenity.py
@@ -1,26 +1,27 @@
 '''
 This module defines the Business Logic Amenity class.
 '''
+from sqlalchemy.orm import validates
 from app.models.base_model import BaseModel
+from app import db
+
 
 class Amenity(BaseModel):
-    def __init__(self, name):
-        super().__init__()
-        self.name = name 
 
-    @property
-    def name(self):
-        return self._name
+    __tablename__ = 'amenities'
 
-    @name.setter
-    def name(self, value):
+    id = db.Column(db.Integer(30), primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    @validates("name")
+    def validate_name(self, key, value):
         if not isinstance(value, str):
             raise TypeError("Name must be a string.")
         if len(value) > 50:
             raise ValueError("Name must be 50 characters maximum.")
         elif len(value) < 1:
             raise ValueError("Name must have at least 1 character.")
-        self._name = value
+        return value
 
     def to_dict(self):
         return {
