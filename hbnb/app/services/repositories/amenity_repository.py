6f8014ddc4ac5a132f6cdfa810d49da4bacaa5cb
Bomays <9314@holbertonstudents.com>
2024-11-19 11:11:35 +0100

    corrected init and import name

--- hbnb/app/services/repositories/amenity_repository.py
+++ hbnb/app/services/repositories/amenity_repository.py
@@ -1,10 +1,10 @@
-from app.models.user import User
+from app.models.amenity import Amenity
 from app import db
 from app.persistence.repository import SQLAlchemyRepository
 
 class AmenityRepository(SQLAlchemyRepository):
     def __init__(self):
-        super().__init__(User)
+        super().__init__(Amenity)
 
     def add(self, amenity):
         db.session.add(amenity)
