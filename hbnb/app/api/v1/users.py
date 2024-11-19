164183ed6d21fda164655db7c43bffc6ab5d394c
Clement Callejon <9397@holbertonstudents.com>
2024-11-19 15:44:47 +0100

    Small refacto

--- hbnb/app/api/v1/users.py
+++ hbnb/app/api/v1/users.py
@@ -90,12 +90,3 @@ class UserResource(Resource):
             return {"error": str(e)}, 400
 
         return updated_user.to_dict(), 204
-
-
-@api.route('/protected')
-class ProtectedResource(Resource):
-    @jwt_required()
-    def get(self):
-        """A protected endpoint that requires a valid JWT token"""
-        current_user = get_jwt_identity()  # Retrieve the user's identity from the token
-        return {'message': f'Hello, user {current_user["id"]}'}, 200
