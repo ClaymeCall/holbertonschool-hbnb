34c7a140ace0b6e67259895e94263d00467c4230
Bomays <9314@holbertonstudents.com>
2024-11-30 19:44:52 +0100

    Added base script file for DOM starting implementing price filter and places list populate functions

--- hbnb/app/static/js/scripts.js
+++ hbnb/app/static/js/scripts.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+  //login form
   const loginForm = document.getElementById('login-form');
 
   if (loginForm) {
@@ -11,10 +12,23 @@ document.addEventListener('DOMContentLoaded', () => {
       await loginUser(email, password);
     });
   }
+
+  //price filter to Index
+  const priceFilter = document.getElementById("price-Filter");
+  if (priceFilter) {
+    populatePriceFilter(priceFilter);
+  }
+
+  //places list to Index
+  const placesList = document.getElementById("places-list");
+  if (placesList) {
+    populatePlacesList(placesList);
+  }
 });
 
+//login function
 async function loginUser(email, password) {
-  const response = await fetch('https://localhost:5001/api/v1/auth/login', {
+  const response = await fetch('http://localhost:5001/api/v1/auth/login', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json'
@@ -29,4 +43,34 @@ async function loginUser(email, password) {
   } else {
     alert('Login failed: ' + response.statusText);
   }
+}
+
+//price filter function
+function populatePriceFilter(priceFilter) {
+  const prices = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000];
+
+  prices.forEach(price => {
+    const option = document.createElement('option');
+    option.value = price;
+    option.textContent = `${price}€`;
+    priceFilter.appendChild(option);
+  });
+}
+
+//place list function
+function populatePlacesList(placesList) {
+  const places = [
+    { name: "Capri Oceanview TinyHouse", price: 200 },
+    { name: "New-York CentralPark RoofTopLoft", price: 1550 },
+    { name: "Montcuq-en-Couseran Bubble Tent Pyrennees view", price: 60 }
+  ];
+
+  places.forEach(place => {
+    const card = document.createElement('div');
+    card.className = 'place-card';
+    card.innerHTML = `<h3>${place.name}</h3>
+    <p>Price per night: ${place.price}€</p>
+    <button class="details-button">View Details</button>`;
+    placesList.appendChild(card);
+  });
 }
