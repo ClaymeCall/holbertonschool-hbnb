b771ebd1b6547b8e1c5109ba595359c98d9741f0
Bomays <9314@holbertonstudents.com>
2024-11-30 19:30:52 +0100

    Added os import, render template from flask, connections to static folder and templates folder, added flask config, changed namespace paths and added HTML routes for index and all others handled in one

--- hbnb/app/__init__.py
+++ hbnb/app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
@@ -26,9 +27,12 @@ authorizations = {
 }
 
 def create_app(config_class="config.DevelopmentConfig"):
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        static_folder=os.path.join(os.path.dirname(__file__), 'static'),
+        template_folder=os.path.join(os.path.dirname(__file__), 'templates')
+    )
     app.config.from_object(config_class)
-
     app.config['JWT_SECRET_KEY'] = app.config.get('SECRET_KEY', 'your_secret_key_here')
 
 
@@ -38,19 +42,33 @@ def create_app(config_class="config.DevelopmentConfig"):
         title='HBnB API',
         description='HBnB Application API',
         security='Bearer Auth',
+        doc='/api/v1/doc',
+        prefix='/api/v1',
         authorizations=authorizations
     )
 
-    api.add_namespace(users_ns, path='/api/v1/users')
-    api.add_namespace(amenities_ns, path='/api/v1/amenities')
-    api.add_namespace(places_ns, path='/api/v1/places')
-    api.add_namespace(reviews_ns, path='/api/v1/reviews')
-    api.add_namespace(auth_ns, path='/api/v1/auth')
+    api.add_namespace(users_ns, path='/users')
+    api.add_namespace(amenities_ns, path='/amenities')
+    api.add_namespace(places_ns, path='/places')
+    api.add_namespace(reviews_ns, path='/reviews')
+    api.add_namespace(auth_ns, path='/auth')
 
     bcrypt.init_app(app)
 
     jwt.init_app(app)
 
     db.init_app(app)
+
+    # HTML pages routes
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+    
+    @app.route('/<page_name>.html')
+    def render_page(page_name):
+        try:
+            return render_template(f'{page_name}.html')
+        except Exception as e:
+            return render_template('404.html'), 404
 
     return app
